409cb1a499b007ec9c48019015a4b550218533d8
User <user@example.com>
2026-07-21 14:28:57 +0530

    Remove unwanted test files

--- web-ui/scripts/check_types.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-    const tools = await prisma.tool.findMany();
-    for (const t of tools) {
-        console.log(t.name + ' - ' + t.type);
-    }
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
--- web-ui/scripts/test_api.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-async function test() {
-    try {
-        // Find a recent STEP file
-        const stepFile = fs.readFileSync('C:\\Projects\\vexcad backup\\web-ui\\public\\test_part.step'); 
-    } catch(e) {}
-}
-test();
